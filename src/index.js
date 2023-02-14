bfd8b7e101c772bc43a9f9912bb3d0c30dc64c93
bastosmkarina <kbastoslab09@gmail.com>
2023-02-14 09:37:19 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
 
-console.log(validator);
+const form = document.getElementById("form")
+const alertCard = document.getElementById("cardvalidation")
+
+form.addEventListener("submit", cardValidator);
+
+function cardValidator(e) {
+  e.preventDefault()
+  const cardNumber = document.getElementById("ccNumber").value;
+  const validresult = validator.isValid(cardNumber);
+  const maskedNumbers = validator.maskify(cardNumber)
+
+  if (cardNumber.length <= 13) {
+    return alertCard.innerHTML = " Ops, o número do cartão está incompleto. ";
+  } else if (validresult === true) {
+    return alertCard.innerHTML = " Tudo certo, o seu cartão de número " + maskedNumbers + " é válido! ";
+  } else {
+    return alertCard.innerHTML = " Ops, cartão inválido, digite novamente. ";
+  }
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,45 @@
 const validator = {
-  // ...
-};
+  isValid (creditCardNumber) {
+    const newArray = [];
+    for(let i = 0; i < creditCardNumber.length; i++) {
+      const numCard = parseInt(creditCardNumber[i]);
+      newArray.push(numCard)
+    }
+
+    const cardNumReverse = newArray.reverse();
+    for (let i = 0; i < cardNumReverse.length; i++) {
+      if ((i +1) % 2 === 0) {
+        cardNumReverse[i] = cardNumReverse[i] * 2;
+      }
+      if (cardNumReverse[i] >= 10) {
+        cardNumReverse[i] = cardNumReverse[i] - 9;
+      }
+    }
+
+    let finalSum = 0
+    for (let i = 0; i < cardNumReverse.length; i++) {
+      finalSum = finalSum + cardNumReverse[i]
+    }
+
+    if (finalSum % 10 === 0) {
+      return true
+    } else {
+      return false
+    }
+  },
+
+  maskify (cardNumber) {
+    const maskNumber = [];
+    for (let i = 0; i < cardNumber.length; i++) {
+      if(i < cardNumber.length - 4) {
+        maskNumber.push("#");
+      } else {
+        maskNumber.push(cardNumber[i])
+      }
+    }
+    const masked = maskNumber.join("");
+    return masked;
+  }
+}
 
 export default validator;
